49ac6249211f98e7a75ae1a75e30f69f9e7e7eb1
wlistertula <tulaa478@gmail.com>
2025-07-13 17:55:23 +0300

    final

--- src/main.tsx
+++ src/main.tsx
@@ -1,28 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import ShopItemFunc from './ShopItemFunc'
-
-const item: ShopItem = {
-  brand: 'Tiger of Sweden',
-  title: 'Leonard coat',
-  description: 'Minimalistic coat in cotton-blend',
-  descriptionFull:
-    "Men's minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.",
-  price: 399,
-  currency: '£',
-};
-
-const ShopApp = () => (
-  <ShopItemFunc
-    item={item}
-    onAddToCart={() => alert(`${item.title} добавлен в корзину!`)}
-  />
-);
+import React, { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
+
 root.render(
   <StrictMode>
-    <ShopApp />
-  </StrictMode>,
+    <App />
+  </StrictMode>
 );
